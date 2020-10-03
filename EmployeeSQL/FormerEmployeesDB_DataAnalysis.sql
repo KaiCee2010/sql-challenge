713c4467c10fb8bd3bec45472a03f15ecc4e80cc
Kaylon <kcyoung@tiksdd.com>
2020-10-02 20:05:04 -0500

    Added sql for data analysis task 5.

--- EmployeeSQL/FormerEmployeesDB_DataAnalysis.sql
+++ EmployeeSQL/FormerEmployeesDB_DataAnalysis.sql
@@ -45,3 +45,13 @@ on a.emp_no = b.emp_no
 JOIN departments c 
 on b.dept_no = c.dept_no
 ORDER BY a.emp_no;
+
+
+-- 5. List first name, last name, and sex for employees whose first name is 
+-- "Hercules" and last names begin with "B."
+SELECT first_name, 
+last_name,
+sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%'
+ORDER BY last_name, sex;
